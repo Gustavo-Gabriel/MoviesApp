c866c5087efce3f352c84bc61cd552a8a385e3fd
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-11-25 16:03:45 -0400

    remove storyboard

--- MoviesApp/AppDelegate.swift
+++ MoviesApp/AppDelegate.swift
@@ -1,19 +1,15 @@
-//
-//  AppDelegate.swift
-//  MoviesApp
-//
-//  Created by Gustavo Anjos on 25/11/21.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- MoviesApp/ViewController.swift
+++ MoviesApp/ViewController.swift
@@ -1,19 +1,11 @@
-//
-//  ViewController.swift
-//  MoviesApp
-//
-//  Created by Gustavo Anjos on 25/11/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
     }
-
-
 }
 
